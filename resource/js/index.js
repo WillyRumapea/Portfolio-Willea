dd51110c9e8f9ce7118e1974d99885e87f4263cb
WillyRumapea <willyrumapea123jr@gmail.com>
2024-08-12 09:46:43 +0700

    fungsional button

---
+++ resource/js/index.js
@@ -0,0 +1,17 @@
+const downloadCV = document.getElementById("download-CV");
+const connectLinkedin = document.getElementById("connect-linkedin");
+
+downloadCV.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.href =
+    "https://drive.google.com/uc?export=download&id=1kpsJJ311gjY-yYeHBeis6K9Gq4f9VJWu";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
+connectLinkedin.addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/willy-rumapea/", "_blank");
+});
+
+//
